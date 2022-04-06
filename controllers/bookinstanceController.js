1bd3a290e975d73c6b4be2c61d71e6e465fe88bd
Cynto <lucagaravello@outlook.com>
2022-04-07 00:01:20 +0200

    Implement get and post requests for updating and deleting items

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,10 +1,15 @@
 const BookInstance = require('../models/bookinstance');
+const Book = require('../models/book');
+const { body, validationResult } = require('express-validator');
+const async = require('async');
 
 // Display list of all BookInstances.
 exports.bookinstance_list = function (req, res, next) {
   BookInstance.find()
     .populate('book')
     .exec(function (err, list_bookinstances) {
+      list_bookinstances.forEach((copy) => console.log(copy));
+
       if (err) {
         return next(err);
       }
@@ -36,31 +41,196 @@ exports.bookinstance_detail = function (req, res, next) {
 };
 
 // Display BookInstance create form on GET.
-exports.bookinstance_create_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance create GET');
+exports.bookinstance_create_get = function (req, res, next) {
+  Book.find({}, 'title').exec(function (err, books) {
+    if (err) {
+      return next(err);
+    }
+    res.render('bookinstance_form', {
+      title: 'Create BookInstance',
+      book_list: books,
+    });
+  });
 };
 
 // Handle BookInstance create on POST.
-exports.bookinstance_create_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance create POST');
-};
+exports.bookinstance_create_post = [
+  body('book', 'Book must be specified').trim().isLength({ min: 1 }).escape(),
+  body('imprint', 'Imprint must be specified')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('status').escape(),
+  body('due_back').optional({ checkFalsy: true }).isISO8601().toDate(),
+
+  function (req, res, next) {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a BookInstance object with escaped and trimmed data.
+    var bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    });
+
+    if (!errors.isEmpty()) {
+      Book.find({}, 'title').exec(function (err, books) {
+        if (err) {
+          return next(err);
+        }
+        // There are errors. Render the form again with sanitized values/error messages.
+        res.render('bookinstance_form', {
+          title: 'Create BookInstance',
+          book_list: books,
+          selected_book: bookinstance.book._id,
+          errors: errors.array(),
+          bookinstance: bookinstance,
+        });
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      bookinstance.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        // Successful - redirect to new record.
+        res.redirect(bookinstance.url);
+      });
+    }
+  },
+];
 
 // Display BookInstance delete form on GET.
-exports.bookinstance_delete_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance delete GET');
+exports.bookinstance_delete_get = function (req, res, next) {
+  async.parallel(
+    {
+      instance: function (callback) {
+        BookInstance.findById(req.params.id).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.instance == null) {
+        // No results.
+        res.redirect('/catalog/bookinstances');
+      }
+      // Successful, so render.
+      res.render('bookinstance_delete', {
+        title: 'Delete Book Instance',
+        instance: results.instance,
+      });
+    }
+  );
 };
 
 // Handle BookInstance delete on POST.
-exports.bookinstance_delete_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance delete POST');
+exports.bookinstance_delete_post = function (req, res, next) {
+  BookInstance.findByIdAndDelete(
+    req.body.instanceid,
+    function deleteInstance(err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/catalog/bookinstances');
+    }
+  );
 };
 
 // Display BookInstance update form on GET.
-exports.bookinstance_update_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance update GET');
+exports.bookinstance_update_get = function (req, res, next) {
+  async.parallel(
+    {
+      instance: (callback) => {
+        BookInstance.findById(req.params.id).populate('book').exec(callback);
+      },
+      books: (callback) => {
+        Book.find(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.instance == null) {
+        // No results.
+        var err = new Error('Bookinstance not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Success.
+      res.render('bookinstance_form', {
+        title: 'Update BookInstance',
+        bookinstance: results.instance,
+        book_list: results.books,
+        selected_book: results.instance.book._id,
+      });
+    }
+  );
 };
 
 // Handle bookinstance update on POST.
-exports.bookinstance_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance update POST');
-};
+exports.bookinstance_update_post = [
+  body('book', 'Book must be specified').trim().isLength({ min: 1 }).escape(),
+  body('imprint', 'Imprint must be specified')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('status').escape(),
+  body('due_back').optional({ checkFalsy: true }).isISO8601().toDate(),
+  function (req, res, next) {
+    const errors = validationResult(req);
+
+    const bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      async.parallel(
+        {
+          instance: (callback) => {
+            BookInstance.findById(req.params.id)
+              .populate('book')
+              .exec(callback);
+          },
+          books: (callback) => {
+            Book.find(callback);
+          },
+        },
+        (err, results) => {
+          if (err) {
+            return next(err);
+          }
+
+          res.render('bookinstance_form', {
+            title: 'Update BookInstance',
+            bookinstance: results.instance,
+            book_list: results.books,
+            selected_book: results.instance.book._id,
+            errors: errors.array(),
+          });
+        }
+      );
+    } else {
+      BookInstance.findByIdAndUpdate(
+        req.params.id,
+        bookinstance,
+        {},
+        (err, theinstance) => {
+          if (err) {
+            return next(err);
+          }
+          res.redirect(theinstance.url);
+        }
+      );
+    }
+  },
+];
--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,7 @@
 const Genre = require('../models/genre');
 const Book = require('../models/book');
 const async = require('async');
+const { body, validationResult } = require('express-validator');
 
 // Display list of all Genre.
 exports.genre_list = function (req, res, next) {
@@ -18,7 +19,7 @@ exports.genre_list = function (req, res, next) {
 };
 
 // Display detail page for a specific Genre.
-exports.genre_detail = function (req, res) {
+exports.genre_detail = function (req, res, next) {
   async.parallel(
     {
       genre: function (callback) {
@@ -50,30 +51,157 @@ exports.genre_detail = function (req, res) {
 
 // Display Genre create form on GET.
 exports.genre_create_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre create GET');
+  res.render('genre_form', { title: 'Create Genre' });
 };
 
 // Handle Genre create on POST.
-exports.genre_create_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre create POST');
-};
+exports.genre_create_post = [
+  // Validate and sanitize the name field.
+  body('name', 'Genre name must not be empty.')
+    .isLength({ min: 1 })
+    .trim()
+    .escape(),
+
+  // Process request after validation and sanitization.
+  function (req, res, next) {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    var genre = new Genre({
+      name: req.body.name,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render('genre_form', {
+        title: 'Create Genre',
+        genre: genre,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      Genre.findOne({ name: req.body.name }).exec(function (err, found_genre) {
+        if (err) {
+          return next(err);
+        }
+
+        if (found_genre) {
+          // Genre exists, redirect to its detail page.
+          res.redirect(found_genre.url);
+        } else {
+          genre.save(function (err) {
+            if (err) {
+              return next(err);
+            }
+            // Genre saved. Redirect to genre detail page.
+            res.redirect(genre.url);
+          });
+        }
+      });
+    }
+  },
+];
 
 // Display Genre delete form on GET.
-exports.genre_delete_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre delete GET');
+exports.genre_delete_get = function (req, res, next) {
+  async.parallel(
+    {
+      genre: function (callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+      genre_books: function (callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.genre == null) {
+        // No results.
+        res.redirect('/catalog/genres');
+      }
+      // Successful, so render.
+      res.render('genre_delete', {
+        title: 'Delete Genre',
+        genre: results.genre,
+        genre_books: results.genre_books,
+      });
+    }
+  );
 };
 
 // Handle Genre delete on POST.
-exports.genre_delete_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre delete POST');
+exports.genre_delete_post = function (req, res, next) {
+  async.parallel(
+    {
+      genre: function (callback) {
+        Genre.findById(req.body.genreid).exec(callback);
+      },
+      genre_books: function (callback) {
+        Book.find({ genre: req.body.genreid }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.author_books.length > 0) {
+        res.render('genre_delete', {
+          title: 'Delete Author',
+          genre: results.genre,
+          genre_books: results.genre_books,
+        });
+        return;
+      } else {
+        Genre.findByIdAndRemove(req.body.genreid, function deleteGenre(err) {
+          if (err) {
+            return next(err);
+          }
+          res.redirect('/catalog/genres');
+        });
+      }
+    }
+  );
 };
 
 // Display Genre update form on GET.
-exports.genre_update_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre update GET');
+exports.genre_update_get = function (req, res, next) {
+  Genre.findById(req.params.id, (err, genre) => {
+    res.render('genre_form', { title: 'Create Genre', genre });
+  });
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre update POST');
-};
+exports.genre_update_post = [
+  body('name', 'Genre name must not be empty.')
+    .isLength({ min: 1 })
+    .trim()
+    .escape(),
+  function (req, res, next) {
+    const errors = validationResult(req);
+
+    const genre = new Genre({
+      name: req.body.name,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('genre_form', {
+        title: 'Create Genre',
+        genre,
+        errors: errors.array(),
+      });
+    } else {
+      Genre.findByIdAndUpdate(req.params.id, genre, {}, (err, thegenre) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(thegenre.url);
+      });
+    }
+  },
+];
